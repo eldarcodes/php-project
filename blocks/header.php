0e717a9bd2a2e52cf0d2edc27475e4a13a52928d
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-03-17 00:31:02 +0200

    Добавлена favicon, переделано переключение по ссылкам в настройках

--- blocks/header.php
+++ blocks/header.php
@@ -11,8 +11,9 @@ $result = mysqli_fetch_assoc($connect);
 
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="/assets/favicon.png" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Мой Веб-сайт</title>
+    <title>PHP project</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
--- blocks/profileSettings.php
+++ blocks/profileSettings.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
 include_once "../scripts/classes.php";
- if(!isset($_SESSION['user'])) {
-     header("location: authorization.php");
- }
+if (!isset($_SESSION['user'])) {
+    header("location: authorization.php");
+}
 include("header.php");
 ?>
 <div class="container">
     <div class="d-flex">
         <div style="height: 70vh;" class="col-3 p-2">
-            <button id="page-one" href="pageone" class="btn btn-primary btn-block mt-2" >Сменить пароль</button>
-            <button id="page-two" class="btn btn-outline-primary btn-block mt-2" >Сменить один</button>
-            <button id="page-three" class="btn btn-outline-primary btn-block mt-2" >Сменить два</button>
-            <button id="page-four" class="btn btn-outline-primary btn-block mt-2" >Сменить три</button>
+            <button id="page-one" href="pageone" class="btn btn-outline-primary btn-block mt-2 settingsButton active">Сменить пароль</button>
+            <button id="page-two" class="btn btn-outline-primary btn-block mt-2 settingsButton">Сменить один</button>
+            <button id="page-three" class="btn btn-outline-primary btn-block mt-2 settingsButton">Сменить два</button>
+            <button id="page-four" class="btn btn-outline-primary btn-block mt-2 settingsButton">Сменить три</button>
         </div>
         <div class="col-9 p-2">
-            <div id="content<?php if(!isset($_SESSION['user'])){echo "NotSession";}?>" class="shadow p-4 bg-white rounded d-block mt-2">
-            <form method="post">
+            <div id="content<?php if (!isset($_SESSION['user'])) {
+                                echo "NotSession";
+                            } ?>" class="shadow p-4 bg-white rounded d-block mt-2">
+                <form method="post">
                     <div class="form-group">
                         <label>Введите старый пароль</label>
                         <input type="password" class="form-control" id="oldPassword" name="oldPassword">
@@ -41,102 +43,77 @@ include("header.php");
             </div>
         </div>
 
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script>
-                $("#changePassword").on('click', function(e) {
-                    var getoldPassword = $("#oldPassword").val();
-                    var newPassword = $("#newPassword").val();
-                    var confirmNewPassword = $("#confirmNewPassword").val();
-                    e.preventDefault();
-                    $.post("changePassword.php", {
-                        "oldPassword": getoldPassword,
-                        "newPassword": newPassword,
-                        "confirmNewPassword": confirmNewPassword,
-                    }, function(data) {
-                        alert(data);
-                    })
-                });
-            </script>
-        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script>
+            $("#changePassword").on('click', function(e) {
+                var getoldPassword = $("#oldPassword").val();
+                var newPassword = $("#newPassword").val();
+                var confirmNewPassword = $("#confirmNewPassword").val();
+                e.preventDefault();
+                $.post("changePassword.php", {
+                    "oldPassword": getoldPassword,
+                    "newPassword": newPassword,
+                    "confirmNewPassword": confirmNewPassword,
+                }, function(data) {
+                    alert(data);
+                })
+            });
+        </script>
     </div>
+</div>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script>
-        $("#page-one").click(function(e) {
-                e.preventDefault();
+<script>
+    let btn = document.querySelectorAll('.settingsButton')
 
-               $.post("changepassword-block.php",{
-               },
-               function(data){
-                   $("#page-one").removeClass('btn-outline-primary');
-                   $("#page-one").addClass('btn-primary');
-                   $("#page-two").removeClass('btn-primary');
-                   $("#page-two").addClass('btn-outline-primary');
-                   $("#page-three").removeClass('btn-primary');
-                   $("#page-three").addClass('btn-outline-primary');
-                                      $("#page-four").removeClass('btn-primary');
-                   $("#page-four").addClass('btn-outline-primary');
-                   $("#content").html(data);
-               }
-               )
-            });
-            $("#page-two").click(function(e) {
-                e.preventDefault();
+    for (let i in btn) {
+        btn[i].onclick = () => {
+            btn.forEach(a => {
+                a.classList.remove('active')
+            })
+            btn[i].classList.add('active')
+        }
+    }
 
-               $.post("content.php",{
-               },
-               function(data){
-                    $("#page-two").removeClass('btn-outline-primary');
-                   $("#page-two").addClass('btn-primary');
-                    $("#page-one").removeClass('btn-primary');
-                   $("#page-one").addClass('btn-outline-primary');
-                   $("#page-three").removeClass('btn-primary');
-                   $("#page-three").addClass('btn-outline-primary');
-                   $("#page-four").removeClass('btn-primary');
-                   $("#page-four").addClass('btn-outline-primary');
-                   $("#content").html(data);
-                  
-               }
-               )
-            });
-            
-            $("#page-three").click(function(e) {
-                e.preventDefault();
+    $("#page-one").click(function(e) {
+        e.preventDefault();
 
-               $.post("page-three.php",{
-               },
-               function(data){
-                $("#page-one").removeClass('btn-primary');
-                   $("#page-one").addClass('btn-outline-primary');
-                   $("#page-two").removeClass('btn-primary');
-                   $("#page-two").addClass('btn-outline-primary');
-                   $("#page-three").removeClass('btn-outline-primary');
-                   $("#page-three").addClass('btn-primary');
-                   $("#page-four").removeClass('btn-primary');
-                   $("#page-four").addClass('btn-outline-primary');
-                   $("#content").html(data);
-               }
-               )
-            });
-            $("#page-four").click(function(e) {
-                e.preventDefault();
+        $.post("changepassword-block.php", {},
+            function(data) {
+                $("#content").html(data);
+            }
+        )
+    });
+    $("#page-two").click(function(e) {
+        e.preventDefault();
 
-               $.post("page-four.php",{
-               },
-               function(data){
-                    $("#page-one").removeClass('btn-primary');
-                   $("#page-one").addClass('btn-outline-primary');
-                   $("#page-two").removeClass('btn-primary');
-                   $("#page-two").addClass('btn-outline-primary');
-                   $("#page-three").removeClass('btn-primary');
-                   $("#page-three").addClass('btn-outline-primary');
-                   $("#page-four").removeClass('btn-outline-primary');
-                   $("#page-four").addClass('btn-primary');
-                   $("#content").html(data);
-               }
-               )
-            });
-    </script>
+        $.post("content.php", {},
+            function(data) {
+                $("#content").html(data);
+
+            }
+        )
+    });
+
+    $("#page-three").click(function(e) {
+        e.preventDefault();
+
+        $.post("page-three.php", {},
+            function(data) {
+                $("#content").html(data);
+            }
+        )
+    });
+    $("#page-four").click(function(e) {
+        e.preventDefault();
+
+        $.post("page-four.php", {},
+            function(data) {
+                $("#content").html(data);
+            }
+        )
+    });
+</script>
 <!--  -->
 <?php
